fefcaed9f83b9e9a3cc64fc0d8ffbab1640c79d5
liz_kang <kangyj@fadutec.com>
2022-10-21 14:14:56 +0900

    ssh 원격 접속 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h2>Jenkins Test</h2>
+      <a href="http://10.16.11.93/">개발 서버</a>
+      <a href="http://10.17.26.8:1234/">원격 서버</a>
     </div>
   );
 }
