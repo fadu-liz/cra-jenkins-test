86ed477a9a6ec507c7d4e3fcf448b8c1a5025344
liz_kang <kangyj@fadutec.com>
2022-10-21 14:32:20 +0900

    링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Jenkins Test</h2>
-      <a href="http://10.16.11.93/">개발 서버</a>
-      <a href="http://10.17.26.8:1234/">원격 서버</a>
+      <div className="Wrapper">
+        <h2>Jenkins Test</h2>
+        <div className="link">
+          <a href="http://10.16.11.93/">개발 서버로 이동</a>
+        </div>
+        <div className="link">
+          <a href="http://10.17.26.8:1234/">원격 서버로 이동</a>
+        </div>
+      </div>
     </div>
   );
 }
